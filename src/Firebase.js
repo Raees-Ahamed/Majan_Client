e18370782b01671ac29e7c56feeb9f9d8b1937ca
Mulitha jayawardana <jmulitha@gmail.com>
2020-09-08 19:33:48 +0530

    third party api integration and third party api authentication

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDXhYPGNSRtrA2WTLcX_vJwG1BHX0Ci2Ig",
+    authDomain: "webapi-react.firebaseapp.com",
+    databaseURL: "https://webapi-react.firebaseio.com",
+    projectId: "webapi-react",
+    storageBucket: "webapi-react.appspot.com",
+    messagingSenderId: "162949073803",
+    appId: "1:162949073803:web:163ee3a0faae090d34bd8e",
+    measurementId: "G-DXE2SC3DZ4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -9,10 +9,11 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from 'axios';
 import * as AppGlobal from "../AppHelp/AppGlobal";
 import Alert from '@material-ui/lab/Alert';
+import firebase from '../Firebase';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -36,39 +37,93 @@ const useStyles = makeStyles((theme) => ({
 
 const Register = () => {
 
-    const [getFirstNameValue, setFirstNameValue] = useState({firstName : ''})
-    const [getLastNameValue, setLastNameValue] = useState({lastName : ''})
-    const [getEmailValue, setEmailValue] = useState({email : ''})
-    const [getPwdValue, setPwdValue] = useState({password : ''})
-    const [getConfPwdValue, setConfPwdValue] = useState({confPassword : ''})
-    const [getUserStatus,setUserStatus] = useState({ status: null });
-    const [getErrorMsg, setErrorMsg] = useState({msg:''});
+    const [getFirstNameValue, setFirstNameValue] = useState({ firstName: '' })
+    const [getLastNameValue, setLastNameValue] = useState({ lastName: '' })
+    const [getEmailValue, setEmailValue] = useState({ email: '' })
+    const [getPwdValue, setPwdValue] = useState({ password: '' })
+    const [getConfPwdValue, setConfPwdValue] = useState({ confPassword: '' })
+    const [getUserStatus, setUserStatus] = useState({ status: null });
+    const [getErrorMsg, setErrorMsg] = useState({ msg: '' });
+    const [getMobileNumberValue, setMobileNumberValue] = useState({ mobileNumber: 0 });
+
+
+
 
     const classes = useStyles();
     let history = useHistory();
 
-    const submitRegisterHandler = () =>{
+    const submitRegisterHandler = () => {
         let formData = {
-            firstName : getFirstNameValue,
-            lastName : getLastNameValue,
-            email : getEmailValue,
-            password : getPwdValue,
-            confPassword : getConfPwdValue,
+            firstName: getFirstNameValue,
+            lastName: getLastNameValue,
+            email: getEmailValue,
+            password: getPwdValue,
+            confPassword: getConfPwdValue,
             usertype: 1
         }
         console.log(formData);
 
-        axios.post(AppGlobal.apiBaseUrl+'User/',  formData )
-            .then(res => {
-                if(res.data.respondId == 0){
-                    setErrorMsg({msg:res.data.description})
-                    setUserStatus({status:0})
-                }else{
-                    setErrorMsg({msg:res.data.description})
-                    setUserStatus({status:1})
-                }
-            })
+        // axios.post(AppGlobal.apiBaseUrl + 'User/', formData)
+        //     .then(res => {
+        //         if (res.data.respondId == 0) {
+        //             setErrorMsg({ msg: res.data.description })
+        //             setUserStatus({ status: 0 })
+        //         } else {
+        //             setErrorMsg({ msg: res.data.description })
+        //             setUserStatus({ status: 1 })
+        //         }
+        //     })
+
+
+        debugger;
+        onSignInSubmit();
+
     }
+
+
+    const setUpRecaptcha = () => {
+        window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recptcha-container', {
+            'size': 'invisible',
+            'callback': function (response) {
+                // reCAPTCHA solved, allow signInWithPhoneNumber.
+                onSignInSubmit();
+            }
+        });
+    }
+
+
+    const onSignInSubmit = () => {
+        debugger
+
+        setUpRecaptcha();
+
+        var phoneNumber = "+94775952461";
+        var appVerifier = window.recaptchaVerifier;
+        firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+            .then(function (confirmationResult) {
+                // SMS sent. Prompt user to type the code from the message, then sign the
+                // user in with confirmationResult.confirm(code).
+                window.confirmationResult = confirmationResult;
+
+                var code = window.prompt("Enter OTP");
+                confirmationResult.confirm(code).then(function (result) {
+                    var user = result.user;
+                    console.log("User signIn " + JSON.stringify(user));
+                }).catch(function (error) {
+                    // User couldn't sign in (bad verification code?)
+                    // ...
+                });
+
+
+
+            }).catch(function (error) {
+                // Error; SMS not sent
+                // ...
+            });
+
+    }
+
+
 
     return (
         <Container component="main" maxWidth="xs">
@@ -145,6 +200,24 @@ const Register = () => {
                                 onChange={event => setConfPwdValue(event.target.value)}
                             />
                         </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="mobileNumber"
+                                label="mobileNumber"
+                                type="number"
+                                id="mobileNumber"
+                                onChange={event => setMobileNumberValue(event.target.value)}
+                            />
+                        </Grid>
+
+
+
+
+
                     </Grid>
 
                     {
@@ -164,7 +237,7 @@ const Register = () => {
                         variant="contained"
                         color="primary"
                         className={classes.submit}
-                        onClick={()=>submitRegisterHandler()}
+                        onClick={() => submitRegisterHandler()}
                     >
                         Sign Up
                     </Button>
@@ -175,6 +248,10 @@ const Register = () => {
                             </Link>
                         </Grid>
                     </Grid>
+
+
+                    <div id="recptcha-container"></div>
+
                 </form>
             </div>
         </Container>
